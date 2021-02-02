ca604e91303c2e10141e40f811e7dce212dc4be6
Chris Barth <chrisjbarth@hotmail.com>
2021-02-02 18:21:32 -0500

    Use class; add ticks support

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ClockOptions } from "../types/clock-options";
+import * as clock from "./clock";
 
 Math.TAU = Math.PI * 2;
 
@@ -15,46 +15,29 @@ const getCanvasContext = (): CanvasRenderingContext2D => {
   }
 };
 
-const getOptions = (): ClockOptions => {
+const getOptions = (): clock.ClockOptions => {
   const options = {
     hResolution: 1920,
     vResolution: 1080,
     clockRadius: 432,
     lineWidth: 10,
     locale: "en-US",
-  } as ClockOptions;
-
-  options.hMidpoint = options.hResolution / 2;
-  options.vMidpoing = options.vResolution / 2;
+    handLength: 200,
+    tickCount: 5,
+    tickHeight: 30,
+    tickWidth: 10,
+  } as clock.ClockOptions;
 
   return options;
 };
 
-const sizeCanvas = (options: ClockOptions): void => {
+const sizeCanvas = (options: clock.ClockOptions): void => {
   const canvas = getCanvas();
   canvas.height = options.vResolution;
   canvas.width = options.hResolution;
 };
 
-const drawClockFrame = (options: ClockOptions): void => {
-  const ctx = getCanvasContext();
-
-  ctx.beginPath();
-  ctx.arc(
-    options.hMidpoint,
-    options.vMidpoing,
-    options.clockRadius,
-    0,
-    Math.TAU,
-    true
-  );
-  ctx.clip();
-  ctx.lineWidth = options.lineWidth;
-  ctx.stroke();
-  ctx.closePath();
-};
-
-const getTime = (seconds: number, options: ClockOptions): string => {
+const getTime = (seconds: number, options: clock.ClockOptions): string => {
   const hours = Math.trunc(seconds / 3600);
   seconds = seconds % 3600;
   const minutes = Math.trunc(seconds / 60);
@@ -67,15 +50,30 @@ const getTime = (seconds: number, options: ClockOptions): string => {
     minute: "numeric",
     second: "numeric",
     timeZone: "UTC",
-    hour12: false,
+    hour12: true,
   };
 
-  const time = new Intl.DateTimeFormat(options.locale, timeOptions).format(
-    date
-  );
+  const timeParts = new Intl.DateTimeFormat(
+    options.locale,
+    timeOptions
+  ).formatToParts(date);
 
-  return time;
+  return timeParts
+    .map((value) => {
+      // Leave off the dayPeriod so we can get a raw time without extra 0s
+      return value.type === "dayPeriod" ? "" : value.value;
+    })
+    .join("")
+    .trim();
 };
 
 const t = getTime(12345, getOptions());
 console.log(t);
+
+sizeCanvas(getOptions());
+
+const myClock = new clock.Clock(getCanvasContext(), getOptions());
+
+myClock.drawFrame();
+myClock.drawHand(1);
+myClock.drawTicks();
--- types/clock-options.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ClockOptions {
-  hResolution: number;
-  vResolution: number;
-  hMidpoint: number;
-  vMidpoing: number;
-  clockRadius: number;
-  lineWidth: number;
-  locale: string;
-}
